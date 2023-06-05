bf7ad6739382e70775f1acf7eada5752999e0f39
Pankaj <pankaj96164041@gmail.com>
2023-06-05 14:53:53 +0530

    array file added

---
+++ array/app.js
@@ -0,0 +1,8 @@
+let name = ["pankaj","monu","sonu","shivsm","pradeep"]
+    console.log (name[3]);
+
+
+    // loop for show all array properties
+    for(let x of name){
+        console.log(x);
+    }
