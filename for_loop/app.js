37c803aa00e5b6c3ffd765ab9d80eab6faa324af
Pankaj <pankaj96164041@gmail.com>
2023-06-05 14:59:47 +0530

    for loop file added

---
+++ for_loop/app.js
@@ -0,0 +1,12 @@
+for (let x = 0; x < 10; x++){
+    console.log ('Pankaj')
+    document.write ('Pankaj')
+    document.write ('<br>')
+}
+
+for (let x = 0; x < 10; x++){
+    console.log ('Monu')
+    document.write ('Monu')
+    document.write('<br>')
+}
+
